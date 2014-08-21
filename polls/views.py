047588c465ea76bfb47a9541360631246c1603e9
ChengYan Yang <e98877331@gmail.com>
2014-08-21 11:40:02 +0800

    first use of template

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from polls.models import Poll
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
+
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll {id}.".format(id = poll_id))
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404  
+    return render(request, 'polls/detail.html', {'poll': poll})
+
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll {id}.".format(id = poll_id))
+    return HttpResponse("You're looking at the results of poll {id}."
+                        .format(id=poll_id))
+
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll {id}.".format(id = poll_id))
-
+    return HttpResponse("You're voting on poll {id}.".format(id=poll_id))
